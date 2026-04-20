2e7f05c54a1b157d1735c5f42e162c79d29ccebd
Alessia795 <alessiapapaz@gmail.com>
2026-04-20 21:47:20 +0100

    Modificado el index.js y añadido a gitignore package-lock.json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,5 +6,21 @@ window.readSeries = function() {
             const seriesList = response.data;
             const seriesID = document.getElementById('series');
 
+            seriesList.forEach(serie => {
+                const tarjeta = document.createElement('div');
+                tarjeta.classList.add("card");
+
+                tarjeta.innerHTML = `
+                <div class = "card-titile">
+                    <h1 class ="card-title"> ${serie.titulo}</h1>
+                    <a href="season.html" >
+                        <img class="card-img" src="http://localhost:8081/${serie.imagen}" alt="Imagen de la serie">
+                    </a>
+                    <p class="card-genre">${serie.genero}</p>
+                    <p class="card-description">${serie.descripcion}</p>
+                </div>
+                `
+                seriesID.appendChild(tarjeta);
+            });
         })
 }
